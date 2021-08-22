cf142cc82217652538b21ef0d00988f6998ccc64
nbarudi <nbarudi@outlook.com>
2021-08-22 16:57:38 -0400

    Starting Team System
    Just creating the files required to make a team system..
    
    The general idea is thought out but nothing is actually planned yet.

---
+++ Hardcore/src/ca/bungo/hardcore/util/managers/TeamManager.java
@@ -0,0 +1,22 @@
+package ca.bungo.hardcore.util.managers;
+
+public class TeamManager {
+	
+	//To-Do: Create a team system
+	/*
+	 * 
+	 * Only team members will be allowed to Teleport to each other
+	 * 
+	 * If admins discover players working together constantly
+	 * 	(Outside of trading / small assists)
+	 * Players will be warnned, kicked, and in worse case banned for not using a team.
+	 * 
+	 * 
+	 * Why enforce a team system?
+	 * 	Players who are on the same team CANNOT claim Kill Bounties on their team mates.
+	 * 	This is to avoid players using exploitable methods to quickly level up them selves or their friends
+	 * 	and to keep players from removing their XP Bounty Punishment caused by killing too many players
+	 * 
+	 */
+
+}
